590891d4f6ce44ff524642b0f7a973aa3c4ef582
BrunoJose-dev <brunojose0112@gmail.com>
2024-05-20 20:35:37 -0300

    Atualização dia 20/05

--- band_finder/front-end/src/components/Projetos-pagUsuario/index.js
+++ band_finder/front-end/src/components/Projetos-pagUsuario/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styles from './Projetos-pagUsuario.module.css';
 
 const ProjetosPage = () => {
   // Estado para armazenar os projetos
