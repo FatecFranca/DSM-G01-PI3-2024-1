5154b4ec2668acf5c247e5487ae5a84e33e5ed33
Allison Rodrigues <allison_rps@hotmail.com>
2024-06-04 21:38:36 -0300

    users page

--- src/components/UsersCard/index.js
+++ src/components/UsersCard/index.js
@@ -19,4 +19,3 @@ const UsersCard = ({ post }) => {
 };
 
 export default UsersCard;
-"UsersCard" - components
--- src/pages/PaginaUsuarios/index.js
+++ src/pages/PaginaUsuarios/index.js
@@ -58,4 +58,3 @@ const UsersPage = () => {
 };
 
 export default UsersPage;
-"PaginaUsuarios"
