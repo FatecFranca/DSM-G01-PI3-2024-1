c72e34665c48e62240afcbc91e2bfce4cf08c627
Allison Rodrigues <allison_rps@hotmail.com>
2024-06-14 11:43:00 -0300

    Finish projeto PI

--- src/pages/PaginaProjetos/index.js
+++ src/pages/PaginaProjetos/index.js
@@ -8,190 +8,50 @@ const ProjetosPage = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    // Simulação de carregamento de dados
-    const mockPosts = [
-      {
-        _id: '662c35fa6b0411a8f1a3bb3c',
-        usuario_id: '662c2ce9e363e968531bb19d',
-        nome: 'Banda Rockers',
-        data: "2024-05-21T00:00:00.000Z",
-        descricao: 'Banda com 5 anos de estrada, com interesse em gravar novo album autoral',
-        midia: 'https://via.placeholder.com/150',
-        municipio: 'Franca',
-        uf: 'SP',
-        procura: 'vocal, bateria',
-        objetivo: 'autoral, cover, hobbie',
-        link: 'instagram.com/banda'
-      },
-      {
-        _id: '66589e305621a584069bcc82',
-        nome: 'Banda de Blues',
-        usuario_id: '6657b3b6a1d206f4a711f6a1',
-        data: "2024-02-29T00:00:00.000Z",
-        descricao: 'Banda de blues procurando novos membros',
-        midia: 'https://via.placeholder.com/150',
-        municipio: 'Porto Alegre',
-        uf: 'RS',
-        procura: 'guitarra',
-        objetivo: 'autoral',
-        link: 'instagram.com/bandablues'
-      },
-      {
-        _id: '66589e185621a584069bcc7e',
-        nome: 'Banda de Pop Rock',
-        usuario_id: '6657b391a1d206f4a711f69d',
-        data: "2024-02-01T00:00:00.000Z",
-        descricao: 'Banda de pop rock com shows regulares',
-        midia: 'https://via.placeholder.com/150',
-        municipio: 'Belo Horizonte',
-        uf: 'MG',
-        procura: 'bateria',
-        objetivo: 'cover',
-        link: 'instagram.com/bandapoprock'
-      },
-      {
-        _id: '66589e7a5621a584069bcc8c',
-        nome: 'Banda de Reggae',
-        usuario_id: '662c2ce9e363e968531bb19d',
-        data: "2024-08-06T00:00:00.000Z",
-        descricao: 'Banda de reggae em busca de novos membros',
-        midia: 'https://via.placeholder.com/150',
-        municipio: 'Franca',
-        uf: 'SP',
-        procura: 'teclado',
-        objetivo: 'hobby',
-        link: 'instagram.com/bandareggae'
-      },
-      {
-        _id: '66589e4b5621a584069bcc86',
-        nome: 'Banda de Rock Progressivo',
-        usuario_id: '6657b3d2a1d206f4a711f6a5',
-        data: "2024-05-21T00:00:00.000Z",
-        descricao: 'Banda de rock progressivo com material próprio',
-        midia: 'https://via.placeholder.com/150',
-        municipio: 'Brasília',
-        uf: 'DF',
-        procura: 'teclado',
-        objetivo: 'autoral',
-        link: 'instagram.com/rockprogressivo'
-      },
-      {
-        _id: '66589dff5621a584069bcc7a',
-        nome: 'Banda Indie',
-        usuario_id: '6657b375a1d206f4a711f699',
-        data: "2024-10-31T00:00:00.000Z",
-        descricao: 'Banda indie buscando novos integrantes',
-        midia: 'https://via.placeholder.com/150',
-        municipio: 'Campinas',
-        uf: 'SP',
-        procura: 'baixo',
-        objetivo: 'autoral',
-        link: 'instagram.com/bandaindie'
-      },
-      {
-        _id: '66589e665621a584069bcc8a',
-        nome: 'Coro Gospel',
-        usuario_id: '6657b3eaa1d206f4a711f6a9',
-        data: "2024-03-25T00:00:00.000Z",
-        descricao: 'Coro gospel com foco em eventos religiosos',
-        midia: 'https://via.placeholder.com/150',
-        municipio: 'Manaus',
-        uf: 'AM',
-        procura: 'piano',
-        objetivo: 'freelance',
-        link: 'instagram.com/corogospel'
-      },
-      {
-        _id: '66589e575621a584069bcc88',
-        nome: 'Dueto Pop',
-        usuario_id: '6657b3dca1d206f4a711f6a7',
-        data: "2024-05-20T00:00:00.000Z",
-        descricao: 'Dueto pop com apresentações regulares',
-        midia: 'https://via.placeholder.com/150',
-        municipio: 'Fortaleza',
-        uf: 'CE',
-        procura: 'baixo',
-        objetivo: 'cover',
-        link: 'instagram.com/duetopop'
-      },
-      {
-        _id: '66589e0c5621a584069bcc7c',
-        nome: 'Orquestra de Câmara',
-        usuario_id: '6657b383a1d206f4a711f69b',
-        data: "2024-02-18T00:00:00.000Z",
-        descricao: 'Orquestra de câmara com repertório variado',
-        midia: 'https://via.placeholder.com/150',
-        municipio: 'São Paulo',
-        uf: 'SP',
-        procura: 'saxofone',
-        objetivo: 'hobby',
-        link: 'instagram.com/orquestradecamara'
-      },
-      {
-        _id: '66589e3e5621a584069bcc84',
-        nome: 'Projeto Acústico',
-        usuario_id: '6657b3c6a1d206f4a711f6a3',
-        data: "2024-06-01T00:00:00.000Z",
-        descricao: 'Projeto acústico com repertório variado',
-        midia: 'https://via.placeholder.com/150',
-        municipio: 'Salvador',
-        uf: 'BA',
-        procura: 'vocal, bateria',
-        objetivo: 'hobby',
-        link: 'instagram.com/projetoacustico'
-      },
-      {
-        _id: '66589db95621a584069bcc78',
-        nome: 'Quarteto de Cordas',
-        usuario_id: '6657b344a1d206f4a711f697',
-        data: "2024-05-03T00:00:00.000Z",
-        descricao: 'Quarteto especializado em música clássica para eventos',
-        midia: 'https://via.placeholder.com/150',
-        municipio: 'Ribeirão Preto',
-        uf: 'SP',
-        procura: 'violino',
-        objetivo: 'freelance',
-        link: 'instagram.com/quartetodecordas'
-      },
-      {
-        _id: '66589e255621a584069bcc80',
-        nome: 'Trio de Jazz',
-        usuario_id: '6657b3a4a1d206f4a711f69f',
-        data: "2024-05-11T00:00:00.000Z",
-        descricao: 'Trio de jazz com foco em eventos corporativos',
-        midia: 'https://via.placeholder.com/150',
-        municipio: 'Curitiba',
-        uf: 'PR',
-        procura: 'piano',
-        objetivo: 'freelance',
-        link: 'instagram.com/triodejazz'
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('https://bandfinder-backend-p669-git-main-allison-rodrigues-projects.vercel.app/projetos');
+        const data = await response.json();
+        console.log('Dados recebidos:', data); // Verifique os dados recebidos
+  
+        // Verifique se `data` é um array
+        if (Array.isArray(data)) {
+          setPosts(data);
+        } else {
+          console.error('Dados recebidos não são um array:', data);
+        }
+      } catch (error) {
+        console.error('Erro ao buscar os dados:', error);
       }
-      
-      ];
-      
-    setPosts(mockPosts);
+    };
+  
+    fetchPosts();
   }, []);
+  
+  
+  useEffect(() => {
+    console.log('Estado posts:', posts); // Verifique o estado posts
+  }, [posts]);
+  
 
   return (
-
     <div className={styles.container}>
-        <Header/>
+      <Header />
       <h1>PROJETOS</h1>
       <div className={styles.grid}>
-        {posts.map((post) => (
+        {Array.isArray(posts) && posts.map((post) => (
           <ProjetosCard key={post._id} post={post} />
         ))}
-
-        <br/>
-        <br/>
-       <div className={styles.footer}>
-         <Footer />
-
-        </div>
-
       </div>
+      <div className={styles.footer}>
+        <Footer />
+      </div>
+      <br/>
+      <br/>
     </div>
   );
 };
 
 export default ProjetosPage;
+
+
--- src/pages/PaginaUsuarios/index.js
+++ src/pages/PaginaUsuarios/index.js
@@ -8,142 +8,21 @@ const UsersPage = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    // Simulação de carregamento de dados
-    const mockPosts = [
-      {
-        _id: "6657b3a4a1d206f4a711f69f",
-        nome: "Ana Clara Martins",
-        cidade: "Curitiba",
-        uf: "PR",
-        influencias: "Jazz, MPB",
-        habilidades: "Saxofone",
-        bio: "Saxofonista de jazz e música brasileira",
-        instagram: "@anasax",
-        facebook: "ana.clara",
-        youtube: "anasaxplayer",
-        midia: '/uploads/ana.jpg'
-      },
-      {
-        _id: "6657b3c6a1d206f4a711f6a3",
-        nome: "Bruna Alves",
-        cidade: "Salvador",
-        uf: "BA",
-        influencias: "Funk, Soul",
-        habilidades: "Baixo",
-        bio: "Baixista em busca de novas colaborações",
-        instagram: "@brunabass",
-        facebook: "bruna.alves",
-        youtube: "brunabassist",
-        midia: '/uploads/bruna.jpg'
-      },
-      {
-        _id: "6657b3dca1d206f4a711f6a7",
-        nome: "Camila Rocha",
-        cidade: "Fortaleza",
-        uf: "CE",
-        influencias: "Pop, Rock",
-        habilidades: "Vocal",
-        bio: "Cantora com experiência em bandas de pop rock",
-        instagram: "@camilarock",
-        facebook: "camila.rocha",
-        youtube: "camilarocha",
-        midia: '/uploads/camila.jpg'
-      },
-      {
-        _id: "6657b375a1d206f4a711f699",
-        nome: "Carlos Souza",
-        cidade: "Campinas",
-        uf: "SP",
-        influencias: "Rock, Reggae",
-        habilidades: "Bateria",
-        bio: "Baterista com experiência em várias bandas de rock",
-        instagram: "@carlosdrums",
-        facebook: "carlos.souza",
-        youtube: "carlosdrummer",
-        midia: '/uploads/carlos.jpg'
-      },
-      {
-        _id: "6657b391a1d206f4a711f69d",
-        nome: "Felipe Santos",
-        cidade: "Belo Horizonte",
-        uf: "MG",
-        influencias: "Pop, Soul",
-        habilidades: "Teclado, Backvocal",
-        bio: "Tecladista e vocalista, pronto para novos desafios",
-        instagram: "@felipemusic",
-        facebook: "felipe.santos",
-        youtube: "felipesantos",
-        midia: '/uploads/felipe.jpg'
-      },
-      {
-        _id: "6657b3eaa1d206f4a711f6a9",
-        nome: "Gabriel Oliveira",
-        cidade: "Manaus",
-        uf: "AM",
-        influencias: "Gospel, Soul",
-        habilidades: "Teclado, Vocal",
-        bio: "Tecladista e cantor com foco em música gospel",
-        instagram: "@gabrielgospel",
-        facebook: "gabriel.oliveira",
-        youtube: "gabrieloliveira",
-        midia: '/uploads/gabriel.jpg'
-      },
-      {
-        _id: "6657b3b6a1d206f4a711f6a1",
-        nome: "João Silva",
-        cidade: "Porto Alegre",
-        uf: "RS",
-        influencias: "Blues, Rock",
-        habilidades: "Guitarra, Vocal",
-        bio: "Guitarrista e vocalista de banda de blues",
-        instagram: "@joaoblues",
-        facebook: "joao.silva",
-        youtube: "joaosilvablues",
-        midia: '/uploads/joao.jpg'
-      },
-      {
-        _id: "6657b383a1d206f4a711f69b",
-        nome: "Larissa Mendes",
-        cidade: "São Paulo",
-        uf: "SP",
-        influencias: "Folk",
-        habilidades: "Violino",
-        bio: "Violinista clássica à procura de novos projetos",
-        instagram: "@larissaviolin",
-        facebook: "larissa.mendes",
-        youtube: "larissaviolinist",
-        midia: '/uploads/larissa.jpg'
-      },
-      {
-        _id: "6657b344a1d206f4a711f697",
-        nome: "Mariana Ferreira",
-        cidade: "Ribeirão Preto",
-        uf: "SP",
-        influencias: "Pop, Jazz",
-        habilidades: "Piano, Vocal",
-        bio: "Cantora e pianista buscando novas oportunidades",
-        instagram: "@mariana.music",
-        facebook: "mariana.music",
-        youtube: "marianamusic",
-        midia: '/uploads/mariana.jpg'
-      },
-      {
-        _id: "6657b3d2a1d206f4a711f6a5",
-        nome: "Ricardo Lima",
-        cidade: "Brasília",
-        uf: "DF",
-        influencias: "MPB",
-        habilidades: "Violão",
-        bio: "Violonista com foco em música brasileira",
-        instagram: "@ricardoviolao",
-        facebook: "ricardo.lima",
-        youtube: "ricardolima",
-        midia: '/uploads/ricardo.jpg'
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('https://bandfinder-backend-p669-git-main-allison-rodrigues-projects.vercel.app/usuarios');
+        const data = await response.json();
+        console.log('Dados recebidos:', data); // Verifique os dados recebidos
+        setPosts(data);
+      } catch (error) {
+        console.error('Erro ao buscar os dados:', error);
       }
-    ];
-    
-    setPosts(mockPosts);
+    };
+  
+    fetchPosts();
   }, []);
+
+
 
   return (
     <div className={styles.container}>
@@ -153,14 +32,15 @@ const UsersPage = () => {
         {posts.map((post) => (
           <UsersCard key={post._id} post={post} />
         ))}
-        <div className={styles.footer}>
-          <Footer />
-        </div>
       </div>
+      <div className={styles.footer}>
+        <Footer />
+      </div>
+      <br/>
+      <br/>
     </div>
   );
 };
-
 
 
 export default UsersPage;
